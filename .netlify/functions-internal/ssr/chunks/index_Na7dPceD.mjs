f78c531e7d711e9e62c7b003db340dc3cb87a5ec
unknown <aaronacevedo94@gmail.com>
2024-02-12 16:35:07 +0100

    cambios a server

--- .netlify/functions-internal/ssr/chunks/index_Na7dPceD.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./prerender_i1K35SxW.mjs');
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'hybrid',
+  output: 'server',
   adapter: netlify(),
 });
 
